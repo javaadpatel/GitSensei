f52f86460485f4673636ff672491d1aec063877c
ethanae <eemandien@protonmail.com>
2019-12-28 12:28:48 +0200

    - add all build tools -- webpack and WDS -- configure webpack -- babel and ts loaders - add ts config for front-end - add initial index component

---
+++ packages/front-end/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const devMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'sensei.bundle.js',
+    chunkFilename: '[name].bundle.js',
+  },
+  devServer: {
+    // open: true,
+    hot: true,
+    historyApiFallback: {
+      index: '/'
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  stats: {
+    chunks: true,
+    chunkModules: false
+  },
+  devtool: devMode && 'inline-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+};
