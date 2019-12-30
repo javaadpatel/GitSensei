bc1727bccffe2b0e18dd2c69eeb98b55f799c80a
ethanae <eemandien@protonmail.com>
2019-12-30 10:16:48 +0200

    - add env to build and dev scripts - webpack config obj to function

--- packages/front-end/webpack.config.js
+++ packages/front-end/webpack.config.js
@@ -2,72 +2,73 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDevMode = process.env.NODE_ENV === 'development';
-
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'sensei.bundle.js',
-    chunkFilename: '[name].bundle.js',
-  },
-  devServer: {
-    // open: true,
-    hot: true,
-    historyApiFallback: {
-      index: '/'
+module.exports = env => {
+  const isProd = env.production;
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'sensei.bundle.js',
+      chunkFilename: '[name].bundle.js',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+    devServer: {
+      // open: true,
+      hot: true,
+      historyApiFallback: {
+        index: '/'
       },
-      {
-        test: /\.tsx$/,
-        enforce: 'pre',
-        use: [
-          {
-            loader: 'tslint-loader',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.tsx$/,
+          enforce: 'pre',
+          use: [
+            {
+              loader: 'tslint-loader',
+              options: {
+                emitErrors: true,
+              }
+            }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              emitErrors: true,
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
             }
           }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react'
-            ]
-          }
-        }
-      },
-    ],
-  },
-  optimization: {
-    minimize: isDevMode,
-    splitChunks: {
-      chunks: 'all',
+        },
+      ],
     },
-  },
-  stats: {
-    chunks: true,
-    chunkModules: false
-  },
-  devtool: isDevMode && 'inline-source-map',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
+    optimization: {
+      minimize: isProd,
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    stats: {
+      chunks: true,
+      chunkModules: false
+    },
+    devtool: !isProd && 'inline-source-map',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
+  }
 };
